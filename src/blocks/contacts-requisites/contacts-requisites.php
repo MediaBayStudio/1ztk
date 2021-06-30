851d502acafd0852d79b7498ba28c3398b31555d
MediaBay <med.bay@yandex.ru>
2021-06-30 18:53:15 +0300

    requisites sect

---
+++ src/blocks/contacts-requisites/contacts-requisites.php
@@ -0,0 +1,4 @@
+<section class="contacts-requisites-sect sect container lazy" data-src="#">
+  <h2 class="contacts-requisites-sect__title sect-title sect-title-underline"><?php echo $section['title'] ?></h2>
+  <p class="contacts-requisites-sect__descr"><?php echo $section['descr'] ?></p>
+</section>
